cf57b10fcf9c5f22d57954c17070a841eeca7d70
Akhil <legitthug54@gmail.com>
2020-11-11 04:44:19 -0500

    minor change to url

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, fireEvent, cleanup, screen } from '@testing-library/react';
 import App from './App';
-import Game from '../Game'
+import Game from '../GameTest'
 import "@testing-library/jest-dom"
 
 afterEach(() => {
